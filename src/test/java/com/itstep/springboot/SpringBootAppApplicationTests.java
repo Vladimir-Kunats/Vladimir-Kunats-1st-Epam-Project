3a0f819de741991219466f7a5bd1d77e03acab1b
Yaraslau Fedarovich <yaraslau_fedarovich@epam.com>
2021-11-22 19:05:19 +0300

    hello from epam

---
+++ src/test/java/com/itstep/springboot/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstep.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
